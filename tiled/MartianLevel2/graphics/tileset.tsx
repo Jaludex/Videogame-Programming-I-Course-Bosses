045c47b58fc4ee4865664bd5f0a80810501c8fb2
Jaludex <jesusleonu2@gmail.com>
2026-08-31 02:18:41 -0400

    Martian: Add level2.json 1st design

---
+++ tiled/MartianLevel2/graphics/tileset.tsx
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset" tilewidth="16" tileheight="16" tilecount="77" columns="7" objectalignment="topleft">
+ <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
+ <image source="tileset.png" width="112" height="176"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collision" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="collision" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="collision" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="collision" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="collision" value="platform"/>
+  </properties>
+ </tile>
+ <wangsets>
+  <wangset name="martianTerrain" type="corner" tile="-1">
+   <wangcolor name="cloud" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="sky" color="#00e9ff" tile="-1" probability="1"/>
+   <wangcolor name="brick" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="15" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="17" wangid="0,0,0,3,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,0,3,0,3,0,0"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="24" wangid="0,3,0,3,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="26" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="31" wangid="0,3,0,0,0,0,0,0"/>
+   <wangtile tileid="32" wangid="0,3,0,0,0,0,0,3"/>
+   <wangtile tileid="33" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="51" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="52" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="53" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="58" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="59" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="60" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="65" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="66" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="67" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="72" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="73" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="74" wangid="0,2,0,2,0,2,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
